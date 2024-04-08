da7f5cd10da5a638579952dc9849c7a05d625098
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-07 22:29:51 -0300

    locale config

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,15 @@ if Rails.env.development?
   puts "cleaning database..."
 
   Country.destroy_all
+  User.destroy_all
 
   #---------- COUNTRIES
   puts "creating countries..."
   Country.create!(name: 'Brazil', code: 'BR')
   Country.create!(name: 'France', code: 'FR')
+
+  #------------ USERS
+  puts "creating users..."
+  User.create!(first_name: 'Branquinho', last_name: 'Chat',
+               user_type: 'landlord', email: 'branquinho@email.com', password: '123123')
 end
