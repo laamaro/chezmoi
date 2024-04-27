c30def74c363dbc488c3bcc75d3a31040afe9ff2
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-27 14:07:17 -0300

    add missing fields to property

---
+++ db/migrate/20240427164817_add_extra_fields_to_property.rb
@@ -0,0 +1,12 @@
+class AddExtraFieldsToProperty < ActiveRecord::Migration[7.0]
+  def change
+    add_column :properties, :children, :boolean
+    add_column :properties, :elevator, :boolean
+    add_column :properties, :smoke_detector, :boolean
+    add_column :properties, :parking, :boolean
+    add_column :properties, :check_in, :time
+    add_column :properties, :check_out, :time
+    add_column :properties, :price_per_night, :float
+    add_column :properties, :maximum_stay, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_09_182035) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_27_164817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_09_182035) do
     t.integer "maximum_guests"
     t.bigint "user_id", null: false
     t.bigint "country_id", null: false
+    t.boolean "children"
+    t.boolean "elevator"
+    t.boolean "smoke_detector"
+    t.boolean "parking"
+    t.time "check_in"
+    t.time "check_out"
+    t.float "price_per_night"
+    t.integer "maximum_stay"
     t.index ["country_id"], name: "index_properties_on_country_id"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
