36e34169175c4e9c7ea62f347a120dc19aa25149
Laura Amaro <lauraamaro1804@gmail.com>
2024-04-27 14:12:12 -0300

    routes for booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :properties
+  resources :properties do
+    resources :bookings, only: %i[index new create]
+  end
+
+  resources :bookings, except: %i[index new create]
 end
